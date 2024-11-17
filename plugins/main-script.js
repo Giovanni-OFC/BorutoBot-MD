00557f4b53991e020e680160e2cee307ca312d97
Giovanni-OFC <jostingaleano10@gmail.com>
2024-11-17 01:00:16 -0600

    Actualizar main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args }) => {
   try {
-    let res = await fetch('https://api.github.com/repos/Diomar-s/Kakaroto-Bot-MD');
+    let res = await fetch('https://api.github.com/repos/Jostin-444');
     if (!res.ok) throw new Error('Error al obtener datos del repositorio');
     let json = await res.json();
 
